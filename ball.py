a4b72fb5122df3dba8a6144c85d071fcf9315031
Felipe Vieira <87919419+UFOsHuygens@users.noreply.github.com>
2021-08-05 01:14:50 -0300

    Update ball.py

--- ball.py
+++ ball.py
@@ -10,35 +10,63 @@ GameLayout:
             rgba: 1, 1, 1, 1
         Rectangle:
             size: self.size
-    AsyncImage:
-        source: "https://image.flaticon.com/icons/png/512/2128/2128322.png"
-        size_hint: 0.1, 0.1
+    Image:
+        source: "ball.png"
+        size_hint: 0.15, 0.15
+        x: 150
+        y: 150
         id: ball
+    Scatter
+        id: obstacle
+        x: 150
+        size_hint: 0.30, 0.05
+        canvas:
+            Color:
+                rgba: 0, 0, 0, 1
+            Rectangle:
+                size: self.size
 '''
 
 class GameLayout(FloatLayout):
-    pos_x = 5
-    pos_y = 1.5
+    pos_x = 25
+    pos_y = 5
 
     def __init__(self, **kwargs):
         super(GameLayout, self).__init__(**kwargs)
         Clock.schedule_interval(self.my_callback, 0.001)  # chamada da função callback a cada 0.001 segundos
 
     def my_callback(self, dt):  # função que movimenta objeto
-        self.ids.ball.y += self.pos_y
-        self.ids.ball.x += self.pos_x
+        ball = self.ids.ball
+        obs = self.ids.obstacle
+        ball.y += self.pos_y
+        ball.x += self.pos_x
 
-        if self.ids.ball.x >= self.width - self.ids.ball.width: # colisão da bola com a parede direita
-            self.pos_x += -5
-        if self.ids.ball.x < 0: # colisão da bola com a parede esquerda
-            self.pos_x += 5
+        if ball.x >= self.width - ball.width: # colisão da bola com a parede direita
+            self.pos_x += -25
+        if ball.x < 0: # colisão da bola com a parede esquerda
+            self.pos_x += 25
 
-        if self.ids.ball.y >= self.height-self.ids.ball.height: # colisão da bola com o teto
-            self.pos_y += -1.5
-        if self.ids.ball.y < 0: # colisão da bola com o chão
-            self.pos_y += 1.5
+        if ball.y >= self.height - ball.height: # colisão da bola com o teto
+            self.pos_y += -5
+        if ball.y < 0: # colisão da bola com o chão
+            self.pos_y += 5
+
         
-        
+        # colisão com o obstaculo
+
+        if ball.x < obs.x + obs.width and \
+            ball.x + ball.width > obs.x and \
+                ball.y < obs.y + obs.height and \
+                    ball.y + ball.height > obs.y:
+                        if ball.x >= obs.x - ball.width:
+                            self.pos_x += -(self.pos_x*2)
+                        if ball.x == obs.x + obs.width - ball.width:
+                            self.pos_x += (self.pos_x*2)
+                        if ball.y >= obs.y - ball.height:
+                            self.pos_y += -(self.pos_y*2)
+                        if ball.y == obs.y + obs.height - ball.height:
+                            self.pos_y += (self.pos_y*2)
+
 class Kivy(App):
     def build(self):
         return Builder.load_string(kv)
